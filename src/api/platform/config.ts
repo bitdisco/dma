1d62b536fb10e643ffda19d60172d64064362404
zhuhl <421973703@qq.com>
2021-06-01 14:22:17 +0800

    修改接口地址

--- src/api/platform/config.ts
+++ src/api/platform/config.ts
@@ -3,10 +3,11 @@
  * @Autor: 朱海良
  * @Date: 2021-03-16 14:20:47
  * @LastEditors: 朱海良
- * @LastEditTime: 2021-06-01 14:15:54
+ * @LastEditTime: 2021-06-01 14:19:39
  */
 import api from "@/api";
 import { AppModule } from "@/store";
+const serviceName = 'platform';
 export default {
   /**
    * 获取平台配置
@@ -15,7 +16,7 @@ export default {
   getConfiguration(moduleCode?: string) {
     moduleCode =
       moduleCode || AppModule.appConfig.moduleCode || "Module.Platform";
-    return api.get(api.getUrl("platform/config", { moduleCode }));
+    return api.get(api.getAbsoluteUrl(serviceName,"platform/config", { moduleCode }));
   },
   /**
    * 获取系统配置
@@ -24,50 +25,50 @@ export default {
   getSystemConfig(moduleCode?: string) {
     moduleCode =
       moduleCode || AppModule.appConfig.moduleCode || "Module.Platform";
-    return api.get(api.getUrl("platform/system_config", { moduleCode }));
+    return api.get(api.getAbsoluteUrl(serviceName,"platform/system_config", { moduleCode }));
   },
   /**
    * 获取配置
    * @param params
    */
   applicationConfiguration: (params: any) =>
-    api.get(api.getUrl("platform/application_config"), params),
+    api.get(api.getAbsoluteUrl(serviceName,"platform/application_config"), params),
 
   /**
    * 获取所有用户列表
    * @param params
    */
   getAllUsers: (params: any) =>
-    api.get(api.getUrl("platform/getallusers"), params),
+    api.get(api.getAbsoluteUrl(serviceName,"platform/getallusers"), params),
 
   /**
    * 获取全部组织机构列表
    */
-  getAllOrganizes: () => api.get(api.getUrl("platform/getallorganizes"), {}),
+  getAllOrganizes: () => api.get(api.getAbsoluteUrl(serviceName,"platform/getallorganizes"), {}),
 
   /**
    * 获取所有用户列表
    */
-  getOwnsOrginzes: () => api.get(api.getUrl("platform/getownsorgainizes"), {}),
+  getOwnsOrginzes: () => api.get(api.getAbsoluteUrl(serviceName,"platform/getownsorgainizes"), {}),
 
   /**
    * 获取所有用户列表
    */
   getAllDctionarys: (moduleCode?: string) => {
     moduleCode = moduleCode || "Module.Platform";
-    return api.get(api.getUrl("platform/getalldictionarys", { moduleCode }));
+    return api.get(api.getAbsoluteUrl(serviceName,"platform/getalldictionarys", { moduleCode }));
   },
 
   /**
    * 获取所有地区列表
    */
   getAllAreaList: () => {
-    return api.get(api.getUrl("platform/getallareas"));
+    return api.get(api.getAbsoluteUrl(serviceName,"platform/getallareas"));
   },
   /**
    * 获取所有生产厂站列表
    */
   getAllSiteList: () => {
-    return api.get(api.getUrl("platform/getallsites"));
+    return api.get(api.getAbsoluteUrl(serviceName,"platform/getallsites"));
   },
 };
