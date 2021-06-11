eba5fee000fe2049aa010de7d6dd019a0b70d2bb
zhuhl <421973703@qq.com>
2021-06-11 10:26:24 +0800

    处理空数据图标

--- src/plugins/vxe-table/index.ts
+++ src/plugins/vxe-table/index.ts
@@ -1,11 +1,20 @@
+/*
+ * @Description: 
+ * @Autor: 朱海良
+ * @Date: 2021-03-16 14:20:48
+ * @LastEditors: 朱海良
+ * @LastEditTime: 2021-06-11 10:25:58
+ */
 import { installRenders } from "./renders";
 import { installXRenders } from "./rendersx";
 import { installVxeTableRenders } from "@cr/utils/vxe-table/renders";
+import { emptyRenders } from './empty'
 
 export default {
   install(Vue: any) {
     installVxeTableRenders();
     installRenders();
-    installXRenders()
+    emptyRenders();
+    installXRenders();
   }
 };
